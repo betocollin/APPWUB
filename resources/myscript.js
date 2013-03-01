646d3e186142d87a1cf47b99061645de1ff8270a
betocollin <betocollin@gmail.com>
2013-03-01 15:42:35 +0000

    APPWUB

---
+++ resources/myscript.js
@@ -0,0 +1,3 @@
+if (window.top.location.href == 'http://localhost/SenchaTouch/iPhoneSecond/app.html' || window.top.location.href == 'http://www.iplanit.ie/SMRTBarbados/app.html'){
+    //window.top.location.href = 'http://www.iplanit.ie/clients-area/';
+}
